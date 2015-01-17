d059385a3c9b204b2ad2b383a9af14109566352f
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2015-01-17 10:20:10 +0530

    Satisfied with the code now I can concentrate on the writing part

--- JSON/encode_decode.lua
+++ JSON/encode_decode.lua
@@ -1,7 +1,18 @@
 local cjson = require "cjson"
 local inspect = require "inspect"
 
-local json_with_arrays = '{"admins": { "names": ["superuser"],"roles": ["admins"]},"members": {"names": ["user1","user2"],"roles": ["developers"]}}'
+local json_with_arrays = '{"admins": { "names": ["superuser"],"roles": ["admins"]},"members": {"names": ["user1","user2"],"roles": ["developers"]}}\n'
+
 local json_with_arrays_decoded = cjson.decode(json_with_arrays)
-print(inspect(json_with_arrays)) -- decoded json
-print(json_with_arrays_decoded.admins.names) --members
+local members = json_with_arrays_decoded.members -- members table
+local admins = json_with_arrays_decoded.admins -- admins table
+print("----Encoded JSON----\n")
+print(json_with_arrays)
+print("----Decoded JSON----\n")
+print(inspect(json_with_arrays_decoded)..'\n') -- decoded json
+print("----Members----\n")
+print(inspect(members)..'\n') --members
+print("----Names of members----\n")
+print(members.names[1]) --user1
+print(members.names[2]) -- user2
+print(members.roles[1]) -- the role of the member "developers"
