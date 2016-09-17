e13a417f978b99e01876788f1969c0cb62fafe8e
brickcap <akshatjiwan@gmail.com>
2016-09-17 17:49:20 +0530

    Only formalities left now

--- mas/lua/sign_cert.lua
+++ mas/lua/sign_cert.lua
@@ -10,10 +10,24 @@
 
 -- SSL commands used --
 --For generating the Certificate Authority Key--
--- openssl  genrsa  -des3 -passout pass:abba -out ../utils/test_certs/ca.key 
+-- openssl  genrsa  -des3 -passout pass:abba -out ../utils/test_certs/ca.key 4096
 --For generating the Certificate Authority certificate--
 --openssl req -new -x509 -days 365 -key ../utils/test_certs/ca.key  -passin pass:abba -out ../utils/test_certs/ca.cert -subj "/C=IN/ST=HR/L=GGN/O=wrinq/CN=www.wrinq.com"
+-- Generating the certificate signing request--
+--openssl req -new -key ../utils/test_certs/client.key -out ../utils/test_certs/client.csr -passin pass:rocks -subj "/C=IN/ST=HR/L=GGN/O=wrinq/CN=www.wrinq.com/emailAddress=akshat@wrinq.com"
+
+--Generate a certificate from the csr and CA
+
+--openssl x509 -req -days 365 -in ../utils/test_certs/client.csr -CA ../utils/test_certs/ca.cert -CAkey ../utils/test_certs/ca.key -passin pass:abba -set_serial 01 -out client.crt
+
+
 -- Decoding the certificate subject line --
 -- openssl x509 -noout -subject -in ../utils/test_certs/ca.cert
+
+
+--- executing these commands with lua
+
+--local handle = io.popen("openssl x509 -noout -subject -in ../utils/test_certs/ca.cert")
+-- local read = handle:read()
 
 -- local 
