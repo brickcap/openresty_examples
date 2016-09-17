e4c041f932a6f0f3ee17bd2767750e1aead2c937
brickcap <akshatjiwan@gmail.com>
2016-09-17 12:30:40 +0530

    Running it in foregound

--- mas/lua/home.lua
+++ mas/lua/home.lua
@@ -1,6 +1,7 @@
 local page = ngx.location.capture("/docs/index.html")
+local cjson = require("cjson")
 ngx.header.content_type =
    "text/html; charset=utf-8"
 ngx.header["ETag"] = page.header["ETag"]
-
+ngx.log(ngx.NOTICE,cjson.encode({hello="dolly"}))
 ngx.say(page.body)
