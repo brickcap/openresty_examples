ae2232e02bf09ac20604403c94140006a9404c93
brickcap <akshatjiwan@gmail.com>
2017-02-26 17:14:59 +0530

    Template rendering is done

--- templating/lua/render_template.lua
+++ templating/lua/render_template.lua
@@ -1,4 +1,4 @@
 local template = require("lib/lua-resty-template/lib/resty.template")
 local template_string = ngx.location.capture("/templates/template.html")
 --ngx.log(ngx.ERR,ngx.var.uri)
-ngx.say(template_string.body)
+template.render(template_string.body,{message="hello world"})
