7f8407b47b77c414ec30c9445fa96d5f9bc75468
brickcap <akshatjiwan@gmail.com>
2017-02-26 16:13:50 +0530

    getting the template via locaiton capture

--- templating/lua/render_template.lua
+++ templating/lua/render_template.lua
@@ -1,1 +1,3 @@
-local template = ngx.location.capture("/")
+local template = require("lib/lua-resty-template/lib/resty.template")
+local template_string = ngx.location.capture("/templates/template.html")
+ngx.say(template_string.body)
