ed080fe96dc66ecf2faab675b47e9634defe571d
brickcap <akshatjiwan@gmail.com>
2017-02-26 17:13:36 +0530

    lua access is not requried

---
+++ templating/lua/access/a_template.lua
@@ -0,0 +1,2 @@
+ngx.log(ngx.ERR,"hey")
+ngx.exit(ngx.HTTP_BAD_REQUEST)
--- templating/lua/render_template.lua
+++ templating/lua/render_template.lua
@@ -1,3 +1,4 @@
 local template = require("lib/lua-resty-template/lib/resty.template")
 local template_string = ngx.location.capture("/templates/template.html")
+--ngx.log(ngx.ERR,ngx.var.uri)
 ngx.say(template_string.body)
