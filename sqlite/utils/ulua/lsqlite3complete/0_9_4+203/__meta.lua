9fbbcc3f8832ec1362c6481a788871e1914d3342
brickcap <akshatjiwan@gmail.com>
2017-04-10 09:34:10 +0530

    using sqlite with openresty

---
+++ sqlite/utils/ulua/lsqlite3complete/0_9_4+203/__meta.lua
@@ -0,0 +1,10 @@
+return {
+  description = "lsqlite3complete : A binding for Lua to the SQLite3 database library",
+  homepage = "http://lua.sqlite.org/",
+  license = "MIT",
+  name = "lsqlite3complete",
+  require = {
+    luajit = "2.0"
+  },
+  version = "0.9.4-203"
+}
