b56e04edc1c96d4e7af1ca4feedf1d84dbc6b065
brickcap <akshatjiwan@gmail.com>
2017-04-11 22:57:53 +0530

    problems with lsqlite3complete

--- sqlite/lua/init.lua
+++ sqlite/lua/init.lua
@@ -1,11 +1,13 @@
 cjson = require "cjson"
+package.path = package.path .. ";./utils/ulua/?/init.lua;"
 local sqlite3 = require("./utils/ulua/lsqlite3complete/0_9_4+203/Linux/x64/-lsqlite3complete")
 
 db_json = sqlite3.open("json")
+db_json_ptr = db_json:get_ptr()
 db_json:exec[[
   CREATE TABLE host (id INTEGER PRIMARY KEY, item TEXT );
 ]]
 
-package.path = package.path .. ";./utils/ulua/?/init.lua;"
+
 
 
--- sqlite/lua/test_json.lua
+++ sqlite/lua/test_json.lua
@@ -1,23 +1,25 @@
+local sqlite3 = require("./utils/ulua/lsqlite3complete/0_9_4+203/Linux/x64/-lsqlite3complete")
 local csv  = require("csv/1+103/")
 local file = csv.open("./utils/data/greece_listings.csv",{header=true})
 local file_data = {}
+local db_ptr = sqlite3.open_ptr(db_json_ptr)
 
-db_json:exec[[
-  CREATE TABLE host (id INTEGER PRIMARY KEY, item TEXT );
-]]
-
-local insert_stmt = db_json:prepare("INSERT INTO host VALUES (NULL, ?)")
+local insert_stmt = db_ptr:prepare("INSERT INTO host VALUES (NULL, ?)")
 
 local function insert(data)
-  insert_stmt:bind_values(data)
-  insert_stmt:step()
-  insert_stmt:reset()
+   local r = insert_stmt:bind_values(data)
+   if r~=0 then
+      ngx.log(ngx.ERR,r)      
+   end
+   insert_stmt:step()
+   insert_stmt:reset()
+   ngx.log(ngx.ERR,db_ptr:errmsg())
 end
 
 for field in file:lines() do
-   insert(cjson.encode(data_field))
+   insert(cjson.encode(field))
    table.insert(file_data,field)
 end
 
 ngx.header.content_type = "application/json"
-ngx.say(cjson.encode(file_data))
+ngx.say("done")
