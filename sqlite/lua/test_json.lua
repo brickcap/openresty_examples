21f229e65c954bd0dcffee02683196ffe7a78481
brickcap <akshatjiwan@gmail.com>
2017-04-11 21:28:00 +0530

    Read csv and converted it into a json document

--- sqlite/lua/test_json.lua
+++ sqlite/lua/test_json.lua
@@ -1,5 +1,12 @@
 local sqlite3 = require("./utils/ulua/lsqlite3complete/0_9_4+203/Linux/x64/-lsqlite3complete")
 local csv  = require("csv/1+103/")
+local file = csv.open("./utils/data/greece_listings.csv",{header=true})
+local file_data = {}
+
 db_mem = sqlite3.open_memory()
 
-ngx.say("hooray")
+for field in file:lines() do
+   table.insert(file_data,field)
+end
+ngx.header.content_type = "application/json"
+ngx.say(cjson.encode(file_data))
