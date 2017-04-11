c422639cb78d40fc14c5b3d7de72b7e9d0c2718b
brickcap <akshatjiwan@gmail.com>
2017-04-11 22:27:53 +0530

    working in the global context but not in local?

--- sqlite/lua/init.lua
+++ sqlite/lua/init.lua
@@ -1,4 +1,10 @@
 cjson = require "cjson"
+local sqlite3 = require("./utils/ulua/lsqlite3complete/0_9_4+203/Linux/x64/-lsqlite3complete")
+
+db_json = sqlite3.open("json")
+db_json:exec[[
+  CREATE TABLE host (id INTEGER PRIMARY KEY, item TEXT );
+]]
 
 package.path = package.path .. ";./utils/ulua/?/init.lua;"
 
--- sqlite/lua/test_json.lua
+++ sqlite/lua/test_json.lua
@@ -1,12 +1,23 @@
-local sqlite3 = require("./utils/ulua/lsqlite3complete/0_9_4+203/Linux/x64/-lsqlite3complete")
 local csv  = require("csv/1+103/")
 local file = csv.open("./utils/data/greece_listings.csv",{header=true})
 local file_data = {}
 
-db_mem = sqlite3.open_memory()
+db_json:exec[[
+  CREATE TABLE host (id INTEGER PRIMARY KEY, item TEXT );
+]]
+
+local insert_stmt = db_json:prepare("INSERT INTO host VALUES (NULL, ?)")
+
+local function insert(data)
+  insert_stmt:bind_values(data)
+  insert_stmt:step()
+  insert_stmt:reset()
+end
 
 for field in file:lines() do
+   insert(cjson.encode(data_field))
    table.insert(file_data,field)
 end
+
 ngx.header.content_type = "application/json"
 ngx.say(cjson.encode(file_data))
