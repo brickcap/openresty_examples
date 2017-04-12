3e1c98a6a482015d12543cfc23f297c40700ccfb
brickcap <akshatjiwan@gmail.com>
2017-04-12 11:30:07 +0530

    its just a folder right?

--- sqlite/lua/init.lua
+++ sqlite/lua/init.lua
@@ -1,63 +1,20 @@
 cjson = require "cjson"
 local lfs = require "./utils/ulua/lfs/1_6_3+203/Linux/x64/-lfs"
-local d_root = "./utils/ulua"
-local DIR_SEP="/"
-
-function browseFolder(root)
-   for entity in lfs.dir(root) do
-      if entity~="." and entity~=".." then
-	 local fullPath=root..DIR_SEP..entity
-	 if root:find("luajit") then
-	    return
-	 end
-	 local mode=lfs.attributes(fullPath,"mode")
-	 local win,osx,x86,doc,ex,man,t,ljit = root:find("Windows"),
-	 root:find("OSX"),
-	 root:find("x86"),
-	 root:find("__doc"),
-	 root:find("__examples"),
-	 root:find("manual"),
-	 root:find("__t")
-
-	 local f_cond = win
-	    or osx
-	    or x86
-	    or doc
-	    or ex
-	    or man
-	    or  t
-
-	 if mode=="file" and not f_cond then
-	    --this is where the processing happens. I print the name of the file and its path but it can be any code    
-	    local p_path = ";"..root.."/?.lua"..";"..root.."/?.so"..";"..root.."/init.lua"
-	    local p_cond = package.path:find(p_path,1,true)
-	    
-	    if not p_cond then
-	       package.path = package.path ..p_path
-	    end
-
-	 elseif mode=="directory" then
-	    browseFolder(fullPath);
-	 end
-      end
-   end
-end
-
-browseFolder(d_root)
 
 -- package.path = package.path .. ";./utils/ulua/?/init.lua"
 -- package.path = package.path .. ";./utils/ulua/?/?.lua"
 -- package.path = package.path .. ";./utils/ulua/?"
 -- package.path = package.path .. ";./?"
 
-ngx.log(ngx.ERR,package.path)
-local sqlite3 = require("csv")
+--local lsqlite = require("ljsqlite3")
 
-db_json = sqlite3.open("json")
-db_json_ptr = db_json:get_ptr()
-db_json:exec[[
-  CREATE TABLE host (id INTEGER PRIMARY KEY, item TEXT );
-]]
+-- local sqlite3 = require("-lsqlite3complete")
+
+-- db_json = sqlite3.open("json")
+-- db_json_ptr = db_json:get_ptr()
+-- db_json:exec[[
+--   CREATE TABLE host (id INTEGER PRIMARY KEY, item TEXT );
+-- ]]
 
 
 
