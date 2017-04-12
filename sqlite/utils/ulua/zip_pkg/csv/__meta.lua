812f82df1d7230f71ca191f6225bc2a0930dded7
brickcap <akshatjiwan@gmail.com>
2017-04-12 13:52:14 +0530

    bow chika dow dow

---
+++ sqlite/utils/ulua/zip_pkg/csv/__meta.lua
@@ -0,0 +1,10 @@
+return {
+  description = "csv : CSV and other delimited file reading",
+  homepage = "http://github.com/geoffleyland/lua-csv",
+  license = "MIT/X11",
+  require = {
+    luajit = "2.0",
+    test = "1-103"
+  },
+  version = "1-103"
+}
---
+++ sqlite/utils/ulua/zip_pkg/pl/__meta.lua
@@ -0,0 +1,11 @@
+return {
+  description = "penlight : Lua utility libraries loosely based on the Python standard libraries",
+  homepage = "http://stevedonovan.github.com/Penlight",
+  license = "MIT/X11",
+  name = "pl",
+  require = {
+    lfs = "1.6.3-2",
+    luajit = "2.0"
+  },
+  version = "1.4.1-103"
+}
