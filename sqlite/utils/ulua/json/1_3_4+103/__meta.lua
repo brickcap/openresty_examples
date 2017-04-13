19fc537a55dfb93a5130be96f054787e01c8b2e7
brickcap <akshatjiwan@gmail.com>
2017-04-13 21:03:42 +0530

    added json lib

---
+++ sqlite/utils/ulua/json/1_3_4+103/__meta.lua
@@ -0,0 +1,12 @@
+return {
+  description = "luajson : customizable JSON decoder/encoder",
+  homepage = "http://github.com/harningt/luajson",
+  license = "MIT/X11",
+  name = "json",
+  require = {
+    lpeg = "0.8.1",
+    luajit = "2.0",
+    re = "0.8.1"
+  },
+  version = "1.3.4-103"
+}
